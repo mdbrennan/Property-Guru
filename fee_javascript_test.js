037362e0816fa54f0fc293412848915c2822696c
mbrennan <mdbrennan@yahoo.com>
2013-01-23 16:07:35 +1100

    Question 2 commit
    Comitting question 2 to its own branch

--- fee_javascript_test.js
+++ fee_javascript_test.js
@@ -29,32 +29,6 @@ function assertEquals(expected, input) {
  * - series can be [0..n], not just including values provided as input
  */
  
-function fizzbuzz(range) {
-    var output = [];
-    range.forEach(function(number) {
-        if(number%3 == 0 && number%5 == 0) {
-            output.push("FizzBuzz");
-        } else if(number%3 == 0) {
-            output.push("Fizz");
-        } else if(number%5 == 0) {
-            output.push("Buzz");
-        } else {
-            output.push(number); 
-        }
-    });
-    output = output.join(" ");
-    console.log(output);
-    return output;
-}
-
-
-assertEquals("Fizz 13 14 FizzBuzz 16", fizzbuzz([12, 13, 14, 15, 16]));
-
-
-/**
- * Assumptions:
- * - same as above
- */
  
 function fizzbuzzbazz(range) {
     var conseq = 0;
